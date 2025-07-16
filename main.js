1a59c21eec3caae462e51329c02f85233ca23520
vankass <akk4aga@gmail.com>
2025-07-16 16:02:58 +0300

    Добавлен section features

---
+++ main.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('.features__item-button').forEach((button) => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+        const isOpen = content.classList.contains('open');
+
+        document.querySelectorAll('.features__item-content').forEach((item) =>
+            item.classList.remove('open')
+        );
+        document.querySelectorAll('.features__item-button').forEach((btn) =>
+            btn.classList.remove('open')
+        );
+
+        if (!isOpen) {
+            content.classList.add('open');
+            button.classList.add('open');
+        }
+    });
+});
