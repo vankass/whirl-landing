b8c66d480509db2b9979e8e34495ac664ab640ba
vankass <akk4aga@gmail.com>
2025-07-20 12:14:01 +0300

    Добавлен section explanations

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-document.querySelectorAll('.features__item-button').forEach((button) => {
-    button.addEventListener('click', () => {
-        const content = button.nextElementSibling;
-        const isOpen = content.classList.contains('open');
+document.querySelectorAll(".features__item-button").forEach((button) => {
+  button.addEventListener("click", () => {
+    const content = button.nextElementSibling;
+    const isOpen = content.classList.contains("open");
 
-        document.querySelectorAll('.features__item-content').forEach((item) =>
-            item.classList.remove('open')
-        );
-        document.querySelectorAll('.features__item-button').forEach((btn) =>
-            btn.classList.remove('open')
-        );
+    document
+      .querySelectorAll(".features__item-header")
+      .forEach((item) => item.classList.remove("open"));
+    document
+      .querySelectorAll(".features__item-content")
+      .forEach((item) => item.classList.remove("open"));
+    document
+      .querySelectorAll(".features__item-button")
+      .forEach((btn) => btn.classList.remove("open"));
 
-        if (!isOpen) {
-            content.classList.add('open');
-            button.classList.add('open');
-        }
-    });
+    if (!isOpen) {
+      content.classList.add("open");
+      button.classList.add("open");
+      const header = button.querySelector(".features__item-header");
+      header.classList.add("open");
+    }
+  });
 });
